632d513c6d98e4bf40e86ba944d8a8ea193d3d91
rebugged <dasabhishek0790@gmail.com>
2021-08-28 15:45:52 +0530

    init

---
+++ src/main/java/com/abhi/abhispringjenkins/AbhiSpringJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.abhispringjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhiSpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbhiSpringJenkinsApplication.class, args);
+	}
+
+}
